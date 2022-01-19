29b3b6d39401c3543869601b58ad127fa426146c
Pramit1010 <94965199+Pramit1010@users.noreply.github.com>
2022-01-18 22:41:45 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var first_project = "My Console Project"
+console.log(first_project)
+
+var first_name = "Pramit"
+var last_name = "Pradhan"
+console.log(first_name, last_name)
+
+var a = 10;
+console.log(a)
+var b = 15;
+console.log(b)
+
+var add = a+b;
+console.log(add)
